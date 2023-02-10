c8cf070e64e0fd61e412795fe9e20187b17d4b19
yoonisali <yoonismahamoudali@gmail.com>
2023-02-10 09:09:31 -0800

    add Header file

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div id="header">
+        <h1>Jealous Jerseys Inventory Tracker</h1>
+    </div>
+  );
+}
+
+export default Header;
